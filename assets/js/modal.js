03f767d7af4732e2992cfce842fcb41ce9f97867
Ashley Hay <hayashley@msn.com>
2019-09-09 13:56:52 -0700

    added info to project modals

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,17 +1,43 @@
 
 
-var modal = document.getElementById("myModal");
 
 
+
+
+
+// Modal one: chatbot
 var btn = document.getElementById("myBtn");
-
-
-var span = document.getElementsByClassName("close")[0];
-
+var modal = document.getElementById("myModal");
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
+
+// Modal two: daily budget app
+var btn1 = document.getElementById("myBtn1");
+var modal = document.getElementById("myModal1");
+
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+// Modal three: Stacks 
+var btn2 = document.getElementById("myBtn2");
+var modal = document.getElementById("myModal2");
+
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+
+
 
 
 span.onclick = function() {
